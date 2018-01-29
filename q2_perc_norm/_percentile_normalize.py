df7fe305b1e1778db9a40d752f0da0ebb545998b
Claire Duvallet <cduvallet@gmail.com>
2018-01-29 18:25:08 -0500

    First working version

--- q2_perc_norm/_percentile_normalize.py
+++ q2_perc_norm/_percentile_normalize.py
@@ -1,10 +1,95 @@
-from q2_types.feature_table import FeatureTable, RelativeFrequency
-from qiime2 import CategoricalMetadataColumn
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, Claire Duvallet.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 
-def percentile_normalize(table: FeatureTable[RelativeFrequency],
-                         metadata: CategoricalMetadataColumn) -> FeatureTable[RelativeFrequency]:
+# Data types
+import biom
+import qiime2
 
-    # Define an empty function for testing.
-    print("Nothing")
+# Functions
+import pandas as pd
+import numpy as np
+import scipy.stats as sp
 
-    return table
+def percentile_normalize(table: biom.Table,
+                         metadata: qiime2.CategoricalMetadataColumn) -> biom.Table:
+    """
+    Converts an input table with cases and controls into percentiles
+    of control samples.
+
+    Parameters
+    ----------
+    table : biom.Table
+        Feature table with relative abundances. Samples are in columns,
+        features (i.e. OTUs) are in rows.
+    metadata : qiime2.CategoricalMetadataColumn
+        metadata column with samples labeled as "case" or "control".
+        All samples with either label are returned, normalized to the
+        equivalent percentile in "control" samples.
+
+    Returns
+    -------
+    norm_biom : biom.Table
+        A biom table with the normalized data, only including the samples
+        that were labeled as either "case" or "control".
+    """
+    # Filter metadata to only include IDs present in the table.
+    # Also ensures every distance table ID is present in the metadata.
+    metadata = metadata.filter_ids(table.ids(axis='sample'))
+    metadata = metadata.drop_missing_values()
+
+    # filter the distance matrix to exclude samples that were dropped from
+    # the metadata due to missing values
+    table = table.filter(metadata.ids)
+
+    metadata = metadata.to_series()
+
+    ## Convert biom Table into dense pandas dataframe
+    # Transpose to samples are in rows and OTUs/features in columns
+    df = table.to_dataframe().to_dense().T
+    x = df.values
+
+    # Get case and control samples from metadata
+    control_samples = metadata[metadata == "control"].index.tolist()
+    case_samples = metadata[metadata == "case"].index.tolist()
+
+    # TODO: make this an optional parameter
+    N_control_thresh = 10
+    if len(control_samples) < N_control_thresh:
+        raise ValueError("There aren't enough controls in your data.")
+
+    # TODO: perform filtering for OTU presence in X% of cases or controls
+
+    # Using numpy is faster than pandas, so get the indices of samples
+    control_indices = [df.index.get_loc(i) for i in control_samples]
+    case_indices = [df.index.get_loc(i) for i in case_samples]
+
+    all_samples = control_samples + case_samples
+    all_indices = control_indices + case_indices
+
+    ## Normalize control and case samples to percentiles of control distribution
+    # j iterates over samples (rows), i iterates over OTUs/features (columns)
+    norm_x = np.array(
+        [
+            [sp.percentileofscore(x[control_indices, i], x[j, i], kind='mean')
+             for j in all_indices]
+        for i in range(x.shape[1])
+        ]).T
+
+    ## Put back into dataframe and convert back to biom format
+    # Transpose it as well, so that samples are back in columns
+    norm_df = pd.DataFrame(
+        data=norm_x,
+        columns=df.columns,
+        index=all_samples).T
+    # Put this dataframe into biom format
+    norm_biom = biom.Table(
+        data=norm_df.values,
+        observation_ids=norm_df.index,
+        sample_ids=norm_df.columns)
+
+    return norm_biom
--- q2_perc_norm/plugin_setup.py
+++ q2_perc_norm/plugin_setup.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, Claire Duvallet.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from qiime2.plugin import Plugin, Metadata, MetadataColumn, Categorical
 #from qiime2.metadata import MetadataColumn, Categorical
 from q2_types.feature_table import FeatureTable, RelativeFrequency
@@ -8,20 +16,23 @@ from q2_perc_norm._percentile_normalize import percentile_normalize
 plugin = Plugin(
     name='perc_norm',
     version=q2_perc_norm.__version__,
-    website='http://www.github.com/cduvallet',
+    website='http://www.github.com/cduvallet/q2-perc-norm',
     package='q2_perc_norm',
-    citation_text=('Percentile normalization does stuff and is published somewhere...'),
-    description=('This QIIME 2 plugin percentile-normalizes '
-                 'case samples relative to controls.'),
+    citation_text=('Sean Gibbons, Claire Duvallet, and Eric Alm. '
+                   '"Correcting for batch effects in case-control '
+                   'microbiome studies". bioRxiv (2017) '
+                   'https://doi.org/10.1101/165910'),
+    description=('This QIIME 2 plugin performs a model-free normalization '
+                 'procedure where features (i.e. bacterial taxa) in case '
+                 'samples are converted to percentiles of the equivalent '
+                 'features in control samples within a study prior to pooling data across studies.'),
     short_description='Plugin to percentile-normalize case-control data.',
-    user_support_text=('Users should request support by raising issues '
-                       'on the github repo.')
+    user_support_text=('Raise an issue on the github repo: https://github.com/cduvallet/q2-perc-norm')
 )
 
 #TODO
-# - define a new FeatureTable[PercentileNormalized] SemanticType and update in the output
-# - figure out how the metadata file gets passed in (look at beta diversity)
-# - figure out wtf MetadataColumn[Categorical] is (pandas series?)
+# - maybe: define a new FeatureTable[PercentileNormalized] SemanticType and update in the output
+# - add filters and related optional parameters (e.g. minimum number of controls, minimum detection rate within controls or cases)
 
 plugin.methods.register_function(
     function=percentile_normalize,
@@ -34,7 +45,10 @@ plugin.methods.register_function(
     },
     parameters={'metadata': MetadataColumn[Categorical]
     },
-    parameter_descriptions={'metadata': ('Categorical sample metadata column with "case" and "control" labels.')
+    parameter_descriptions={
+        'metadata': ('Sample metadata column which has samples '
+                     'labeled as "case" or "control". Samples which '
+                     'are not labeled are not included in the output table.')
     },
     output_descriptions={'perc_norm_table': 'The percentile-normalized OTU table.'},
     name='Percentile normalization',
